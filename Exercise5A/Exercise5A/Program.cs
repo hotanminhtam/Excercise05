966e6c8b9b5f7add59b1c6a5105a682b1da546a7
hungdp2612 <hungdp2612@gmail.com>
2019-10-08 17:49:01 +0700

    ok

--- Exercise5A/Exercise5A/Program.cs
+++ Exercise5A/Exercise5A/Program.cs
@@ -10,7 +10,87 @@ namespace Exercise5A
     {
         static void Main(string[] args)
         {
+            Print_a_to_z_v1();
+            Print_z_to_a();
+            while (true)
+            {
+                char c = Console.ReadKey().KeyChar;
+                Console.WriteLine(" " + Char2Code(c));
+            }
+        }
 
+        public static void Print_a_to_z_v1()
+        {
+            char c = ' ';
+            for (int i = 97; i < 122; i++)
+            {
+                c = (char)i;
+                Console.Write(c);
+            }
+            Console.WriteLine();
+        }
+
+        public static void Print_a_to_z_v2()
+        {
+            char c = 'a';
+            for (int i = 0; i < 26; i++)
+            {
+                Console.WriteLine(c);
+                c++;
+            }
+        }
+
+        public static void Print_a_to_z_v3()
+        {
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+        public static void Print_a_to_z_v4()
+        {
+            string c = "abcdefghijklmnopqrstuvwxy";
+            for (int i = 0; i < c.Length; i++)
+            {
+                Console.WriteLine(c[i]);
+            }
+        }
+
+        public static void Print_z_to_a()
+        {
+            char c = ' ';
+            for (int i = 122; i >= 97; i--)
+            {
+                c = (char)i;
+                Console.Write(c);
+            }
+            Console.WriteLine();
+        }
+
+        public static int Char2Code(char c)
+        {
+            return (int)c;
+        }
+
+        public static bool IsUpper(char c)
+        {
+            return false;
+        }
+
+        public static bool IsLower(char c)
+        {
+            return false;
+        }
+
+        public static char ToUpper(char c)
+        {
+            return c;
+        }
+
+        public static char ToLower(char c)
+        {
+            return c;
         }
     }
 }
